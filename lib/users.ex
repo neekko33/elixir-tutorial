d04f12aac476f5ad7c7b3a1ad5695e48e0916559
neekko33 <neekko33@outlook.com>
2025-07-02 23:44:55 +0800

    structs;

--- lib/users.ex
+++ lib/users.ex
@@ -1,23 +1,29 @@
 defmodule Users do
-  alias Helpers.Format
+  @enforce_keys [:name]
+  defstruct [:name, :age, :is_cool, :can_drink]
+
+  import Helpers.Format
+
+  def check(%Users{} = _user), do: true
+  def check(_user), do: false
 
   def new("Ken", age) do
-    %{name: "Ken", age: age, is_cool: true, can_drink: true}
+    %Users{name: "Ken", age: age, is_cool: true, can_drink: true}
   end
 
   def new(name, age) when age >= 18 do
-    formatted_name = Format.trim_text(name)
-    %{name: formatted_name, age: age, is_cool: false, can_drink: true}
+    formatted_name = trim_text(name)
+    %Users{name: formatted_name, age: age, is_cool: false, can_drink: true}
   end
 
   def new(name, age) do
-    formatted_name = Format.trim_text(name)
-    %{name: formatted_name, age: age, is_cool: false, can_drink: false}
+    formatted_name = trim_text(name)
+    %Users{name: formatted_name, age: age, is_cool: false, can_drink: false}
   end
 
   def new, do: generate_default_user()
 
   defp generate_default_user do
-    %{name: "Default User", age: 30, is_cool: false, can_drink: false}
+    %Users{name: "Default User", age: 30, is_cool: false, can_drink: false}
   end
 end
