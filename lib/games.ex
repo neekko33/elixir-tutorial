84afd9edee3b46945a151ef79582c721a37334b9
neekko33 <neekko33@outlook.com>
2025-07-02 22:20:37 +0800

    recursion;

--- lib/games.ex
+++ lib/games.ex
@@ -3,6 +3,27 @@ defmodule Games do
     %{name: "Diablo 4", in_stock: true, price: 20}
   end
 
+  def get_all do
+    [
+      %{name: "Diablo 4", in_stock: true, price: 20},
+      %{name: "Starfield", in_stock: false, price: 60},
+      %{name: "Cyberpunk 2077", in_stock: true, price: 30},
+      %{name: "Hogwarts Legacy", in_stock: true, price: 50}
+    ]
+  end
+
+  def count([]), do: 0
+
+  def count([_ | tail]) do
+    1 + count(tail)
+  end
+
+  def total([]), do: 0
+
+  def total([head | tail]) do
+    head.price + total(tail)
+  end
+
   def purchase(game) do
     case validate_purchase(game) do
       {:error, reason} -> "Error: #{reason}"
